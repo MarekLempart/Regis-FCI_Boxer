164d2b5e66f91dbe0b669f63db2243ad35501971
MarekLempart <kama.zakupy@gmail.com>
2024-11-14 12:23:00 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
                 <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h4>Vite + React</h4>
         </>
     );
 }
