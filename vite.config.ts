660afe0b6268c4821d66276295a38438df2b7f5f
MarekLempart <kama.zakupy@gmail.com>
2024-11-27 12:44:15 +0100

    work BrowserRouter

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Regis-FCI_Boxer/",
+  // base: process.env.NODE_ENV === 'production' ? "/Regis-FCI_Boxer/" : "/",
   plugins: [react()],
 })
