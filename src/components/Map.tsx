a1390577b74e6d0ef2bb78c700ad5dc8df024d19
MarekLempart <kama.zakupy@gmail.com>
2024-12-18 12:51:23 +0100

    Update Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,6 +3,8 @@
 import React, { useEffect } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 import { MapOverlay, MapWrapper, MapCloseButton } from "../styles/MapStyles";
 
 interface MapProps {
@@ -14,13 +16,27 @@ interface MapProps {
 
 const Map: React.FC<MapProps> = ({ latitude, longitude, address, onClose }) => {
   useEffect(() => {
+
+     // Ustawienie niestandardowej ikony
+     const defaultIcon = L.icon({
+        iconUrl: markerIcon,
+        shadowUrl: markerShadow,
+        iconSize: [25, 41], // Domyślny rozmiar Leaflet
+        iconAnchor: [12, 41], // Punkt zaczepienia
+        popupAnchor: [1, -34], // Pozycja popupu względem ikony
+        shadowSize: [41, 41], // Rozmiar cienia
+      });
+
     const map = L.map("leaflet-map").setView([latitude, longitude], 16);
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
     }).addTo(map);
-
-    L.marker([latitude, longitude]).addTo(map).bindPopup(address).openPopup();
+    
+    L.marker([latitude, longitude], { icon: defaultIcon })
+        .addTo(map)
+        .bindPopup(address)
+        .openPopup();
 
     return () => {
       map.remove(); // Usunięcie instancji mapy przy odmontowaniu komponentu
@@ -30,7 +46,7 @@ const Map: React.FC<MapProps> = ({ latitude, longitude, address, onClose }) => {
   return (
     <MapOverlay onClick={onClose}>
       <MapWrapper onClick={(e) => e.stopPropagation()}>
-        <MapCloseButton onClick={onClose}>×</MapCloseButton>
+        <MapCloseButton onClick={onClose}>X</MapCloseButton>
         <div id="leaflet-map" style={{ width: "100%", height: "100%" }} />
       </MapWrapper>
     </MapOverlay>
