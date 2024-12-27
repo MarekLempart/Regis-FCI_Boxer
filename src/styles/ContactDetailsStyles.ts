135180bdfaac04b75d8897e2b16c6cb7198c49e2
MarekLempart <kama.zakupy@gmail.com>
2024-12-27 19:13:52 +0100

    Update ContactDetailsStyles.ts

--- src/styles/ContactDetailsStyles.ts
+++ src/styles/ContactDetailsStyles.ts
@@ -17,12 +17,29 @@ export const SectionTitle = styled.h2`
   font-size: 24px;
   color: ${({ theme }) => theme.colors.distinction};
   margin-bottom: 15px;
+
+  @media ${({ theme }) => theme.media.tablet} {
+    margin-bottom: 30px;
+  }
+
+  @media ${({ theme }) => theme.media.desktop} {
+    margin-bottom: 60px;
+  }
 `;
 
 export const DetailItem = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
+  
+
+  @media ${({ theme }) => theme.media.tablet} {
+    margin-left: 60px;
+  }
+
+  @media ${({ theme }) => theme.media.desktop} {
+    margin-left: 150px;
+  }
 `;
 
 export const DetailIcon = styled.div`
