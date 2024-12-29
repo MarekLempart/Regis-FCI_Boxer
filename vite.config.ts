7ffbb08e55d88d3cea6708184593f687f864b165
MarekLempart <kama.zakupy@gmail.com>
2024-12-29 20:23:44 +0100

    work

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+// import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,10 +9,13 @@ export default defineConfig({
   base: process.env.VITE_DEPLOY_ENV === 'ghpages' ? "/Regis-FCI_Boxer/" : "/",
   plugins: [
     react(),
-    ViteImageOptimizer({
-      png: {
-        quality: 80, // Dostosuj jakość
-      },
-    }),
+    // ViteImageOptimizer({
+    //   png: {
+    //     quality: 80, // Dostosuj jakość
+    //   },
+    //   webp: {
+    //     lossless:true,
+    //   },
+    // }),
   ],
 })
